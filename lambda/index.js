49653b15a8a6a35799b19648f15bba440ce2f707
orumpark94 <nowd940@gmail.com>
2025-04-10 16:12:56 +0900

    CORS 수정

--- lambda/index.js
+++ lambda/index.js
@@ -3,6 +3,8 @@ exports.handler = async (event) => {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',     // ✅ 중요: 사전 요청 허용
+      'Access-Control-Allow-Methods': 'GET,OPTIONS', // ✅ 중요: 허용 메서드
     },
     body: JSON.stringify({ message: "Hello SJ PARK from Lambda!" }),
   };
